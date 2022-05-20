71be313beb3d0d110ef5a63a08e7f256167af00a
josielmorais <moraisjosiel1@gmail.com>
2022-05-20 10:11:41 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/rest_apis/RestApIsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApIsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rest_apis/RestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApIsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
